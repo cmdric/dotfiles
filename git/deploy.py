476f77c529d7b46aeae20ecdc828b08b5930a316
cmdric <cedric.potterat@cern.ch>
2014-05-05 14:43:37 +0200

    add all files

---
+++ git/deploy.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import os
+
+os.system( "git config --global color.ui true" )
+os.system( 'git config --global author.name "Albert Puig"' )
+os.system( 'git config --global author.email "albert.puig@cern.ch"' )
+os.system( 'git config --global push.default simple'  )
