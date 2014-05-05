412bfe5516adc147ff25b7e00bbf69153c968508
Cédric Potterat <cmdric@gmail.com>
2014-05-05 14:30:13 +0200

    remove files

--- git/deploy.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import os
-
-os.system( "git config --global color.ui true" )
-os.system( 'git config --global author.name "Albert Puig"' )
-os.system( 'git config --global author.email "albert.puig@cern.ch"' )
-os.system( 'git config --global push.default simple'  )
